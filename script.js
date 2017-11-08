d2bc37d61c779f08ece078f581ca94e51d7209a1
Yasser Arturo Carreon Lopez <yasser carreon@tiempodev.local>
2017-11-08 14:38:43 -0600

    Isolated scope

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var app = angular.module('app', [])
 
 app.controller('mainCtrl', function ($scope) {
-  $scope.user = {
+  $scope.user1 = {
     name: 'Luke',
     address: {
       street: '123 Main St.'
@@ -9,6 +9,16 @@ app.controller('mainCtrl', function ($scope) {
     friends: [
       'John',
       'Carlos'
+    ]
+  },
+  $scope.user2 = {
+    name: 'Han',
+    address: {
+      street: '123 Main St.'
+    },
+    friends: [
+      'Luke',
+      'Chewbacca'
     ]
   }
   console.log($scope)
@@ -18,6 +28,10 @@ app.directive('userInfoCard', function () {
   return {
     templateUrl: 'templates/userInfoCard.html',
     restrict: 'E',
+    // scope: true, // internal scope
+    scope: {
+      user: '='
+    },
     controller: function ($scope) {      
       $scope.knightMe = function (user) {    
         user.rank = 'knight'
