f63ceda86e8bd9559a23893b6942e6be4bcee5f2
Yasser Arturo Carreon Lopez <yasser carreon@tiempodev.local>
2017-11-09 13:44:07 -0600

    Show messages on pause video

--- script2.js
+++ script2.js
@@ -1,5 +1,24 @@
-app.controller('mainCtrl2', function () {
+app.controller('mainCtrl2', function ($scope) {
+  $scope.messages = []
+  $scope.handlePause = function (e) {
+    console.log(e)
+    $scope.messages.push({ text: 'paused' })
+    console.log('paused')
+  }
+})
 
+app.directive('eventPause', function ($parse) {
+  return {
+    restrict: 'A',
+    link: function (scope, el, attrs) {
+      var fn = $parse(attrs['eventPause'])
+      el.on('pause', function (event) {
+        scope.$apply(function () {
+          fn(scope, { evt: event })
+        })
+      })
+    } 
+  }
 })
 
 app.directive('spacebarSupport', function () {
