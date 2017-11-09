072ff2b37ae0eafe54cbfdf6fd17ba3e7214355f
Yasser Arturo Carreon Lopez <yasser carreon@tiempodev.local>
2017-11-09 14:04:04 -0600

    My own click directive

---
+++ script3.js
@@ -0,0 +1,19 @@
+app.controller('mainCtrl3', function ($scope) {
+  $scope.data = { message: 'I have not been clicked' }
+  $scope.clickHandler = function (p) {
+    p.message = 'I have been clicked'
+  } 
+})
+
+app.directive('myClick', function ($parse) {
+  return {
+    link: function (scope, el, attrs) {
+      var fn = $parse(attrs['myClick'])
+      el.on('click', function () {
+        scope.$apply(function () {
+          fn(scope)
+        })
+      })
+    }
+  }
+})
