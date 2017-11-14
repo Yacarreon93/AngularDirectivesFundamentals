da8f3b133018c78014628c7f7cad87891204d072
Yasser Arturo Carreon Lopez <yasser carreon@tiempodev.local>
2017-11-14 15:54:12 -0600

    Panel directive

---
+++ script12.js
@@ -0,0 +1,46 @@
+var app = angular.module('app', [])
+
+app.controller('mainCtrl', function ($scope) {
+
+})
+
+app.directive('swTabstrip', function () {
+  return {
+    restrict: 'E',
+    transclude: true,
+    scope: {},
+    controller: function ($scope) {
+      $scope.panes = []
+      $scope.select = function (pane) {
+        pane.selected = true
+        $scope.panes.forEach(function(curPane) {
+          if (curPane !== pane) {
+            curPane.selected = false
+          }
+        })
+      }
+      this.addPane = function (pane) {
+        $scope.panes.push(pane)
+        if ($scope.panes.length === 1) {
+          pane.selected = true
+        }
+      }
+    },
+    templateUrl: 'templates/swTabstrip.html'
+  }
+})
+
+app.directive('swPane', function () {
+  return {
+    restrict: 'E',
+    transclude: true,
+    scope: {
+      title: '@'
+    },
+    require: '^swTabstrip',
+    link: function (scope, el, attrs, tabstripCtrl) {
+      tabstripCtrl.addPane(scope)
+    },
+    templateUrl: 'templates/swPane.html'
+  }
+})
