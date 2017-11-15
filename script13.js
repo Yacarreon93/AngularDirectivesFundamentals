9ef5418867a9c6c0c26082e56a6b0bc50d12d242
Yasser Arturo Carreon Lopez <yasser carreon@tiempodev.local>
2017-11-15 16:05:50 -0600

    Using modal directive

--- script13.js
+++ script13.js
@@ -132,10 +132,14 @@ app.directive('personInfoCard', function (jediPolicy) {
     bindToController: true,
     controller: function () {      
       this.knightMe = function (person) {
-        jediPolicy.advanceToKnight(person).then(null, function (user) {
-          alert('Sorry, ' + user.name + ' is not ready to be a Jedi')
-        })
-      }     
+        this.showKnightModal = true
+      }
+      this.knightDialogDone = function (response) {
+        this.showKnightModal = false
+        if (response) {
+          this.person.rank = 'Jedi knight' 
+        }
+      }
       this.removeFriend = function (friend) {
         var idx = this.person.friends.indexOf(friend)
         if (idx > -1) {
@@ -182,6 +186,50 @@ app.directive('address', function () {
       $scope.expandAddress = function () {
         $scope.collapsed = false
       }
+    }
+  }
+})
+
+app.controller('modalCtrl', function ($scope) {
+  $scope.close = function (response) {
+    $scope.closeModal(response)
+  }
+})
+
+app.directive('modal', function ($document) {
+  return {
+    scope: {
+      modalOpen: '=open',
+      options: '=',
+      onClose: '&'
+    },
+    transclude: true,
+    templateUrl: 'templates/modal.html',
+    controller: function ($scope) {
+      $scope.close = function () {
+        $scope.modalOpen = false
+        $scope.onClose()
+      } 
+    },
+    link: function ($scope, el, attrs) {
+      var options = angular.extend({
+        height: '250px',
+        width: '500px',
+        top: '20%',
+        left: '30%'
+      }, $scope.options)
+      el.find('.modal-container').css({
+        left: options.left,
+        top: options.top,
+        height: options.height + 'px',
+        width: options.width + 'px'
+      })
+      var pageHeight = $document.height()
+      var pageWidth = $document.width()
+      el.find('.modal-blackout').css({
+        'width': pageWidth + 'px',
+        'height': pageHeight + 'px'
+      })
     }
   }
 })
